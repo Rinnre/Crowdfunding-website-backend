baa6aeaa301359cc36e436c19200d2450fd586a9
wj <1595519431@qq.com>
2022-06-01 15:53:19 +0800

    v1.6 支付订单基本完成

--- crowd-pay-consumer/src/main/java/com/wj/crowd/pay/controller/PayController.java
+++ crowd-pay-consumer/src/main/java/com/wj/crowd/pay/controller/PayController.java
@@ -5,6 +5,7 @@ import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.domain.AlipayTradePrecreateModel;
 import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.alipay.api.request.AlipayTradePrecreateRequest;
 import com.alipay.api.response.AlipayTradePrecreateResponse;
 import com.wj.crowd.api.mysql.MysqlRemoteService;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -53,11 +55,12 @@ public class PayController {
 
     @ApiOperation(value = "支付宝支付 沙箱环境")
     @PostMapping("/send/request/to/pay/{order_id}")
-    public ResultEntity<String> sendRequestToAliPay(@PathVariable("order_id") String orderId) throws AlipayApiException {
+    public ResultEntity<String> sendRequestToAliPay(@PathVariable("order_id") String orderId
+    ) throws AlipayApiException {
         //调用远程接口获订单信息
         ResultEntity<PayOrder> orderInfoDetailRemote = mysqlRemoteService.getOrderInfoDetailRemote(orderId);
 
-        if(!orderInfoDetailRemote.isSuccess()){
+        if (!orderInfoDetailRemote.isSuccess()) {
             throw new CrowdException(ResultCodeEnum.DATA_ERROR);
         }
 
@@ -73,16 +76,39 @@ public class PayController {
                 payProperties.getAlipayPublicKey(),
                 payProperties.getSignType());
 
-        AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
-        AlipayTradePrecreateModel model = new AlipayTradePrecreateModel();
-        request.setBizModel(model);
-        model.setOutTradeNo(UUID.randomUUID()+"");
-        model.setTotalAmount("88.88");
-        model.setSubject("Iphone6 16G");
-        AlipayTradePrecreateResponse response = alipayClient.execute(request);
-        log.info(response.toString());
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+
+        request.setNotifyUrl(payProperties.getNotifyUrl());
+        request.setReturnUrl(payProperties.getReturnUrl()+"?projectId="+order.getProjectId());
+        //商户订单号，商户网站订单系统中唯一订单号，必填
+        //生成随机Id
+        String out_trade_no = order.getId();
+        //付款金额，必填
+        String total_amount = order.getOrderAmount() + "";
+        //订单名称，必填
+        String subject = "测试";
+        request.setBizContent("{\"out_trade_no\":\"" + out_trade_no + "\","
+                + "\"total_amount\":\"" + total_amount + "\","
+                + "\"subject\":\"" + subject + "\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+//        AlipayTradePrecreateModel model = new AlipayTradePrecreateModel();
+//        request.setBizModel(model);
+//        model.setOutTradeNo(order.getId());
+//        model.setTotalAmount(order.getOrderAmount() + "");
+//        model.setSubject("测试");
+
+
+        String form = "";
+        try {
+            form = alipayClient.pageExecute(request).getBody(); // 调用SDK生成表单
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+
+
         // 返回支付宝支付网址，用于生成二维码
-        return ResultEntity.success(response.getQrCode());
+        return ResultEntity.success(form);
     }
 
     @ApiOperation(value = "支付宝支付 异步通知")
@@ -107,53 +133,60 @@ public class PayController {
                 params, payProperties.getAlipayPublicKey(), payProperties.getCharset(), payProperties.getSignType()); //调用 SDK 验证签名
 
 //        if(signVerified) {
-            //验证成功
-            log.info("验证成功");
-            //商户订单号
-            String outTradeNo =
-                    new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
-            //支付宝交易号
-            String tradeNo =
-                    new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
-            //交易状态
-            String tradeStatus =
-                    new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
-            log.info("out_trade_no="+outTradeNo);
-            log.info("trade_no="+tradeNo);
-            log.info("trade_status="+tradeStatus);
+        //验证成功
+        log.info("验证成功");
+        //商户订单号
+        String outTradeNo =
+                new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+        //支付宝交易号
+        String tradeNo =
+                new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+        //交易状态
+        String tradeStatus =
+                new String(request.getParameter("trade_status").getBytes("ISO-8859-1"), "UTF-8");
+        log.info("out_trade_no=" + outTradeNo);
+        log.info("trade_no=" + tradeNo);
+        log.info("trade_status=" + tradeStatus);
 
 
-
-            if (("TRADE_SUCCESS").equals(tradeStatus)){
-                // 向前端发送一条支付成功的通知
-                webSocket.sendMessage("true");
-                // 更新订单状态
-                PayOrder payOrder = new PayOrder();
-                payOrder.setId(outTradeNo);
-                payOrder.setPayNum(tradeNo);
-                payOrder.setOrderStatus(CrowdConstant.ORDER_STATUS_PENDING_SHIPMENT);
-                ResultEntity<String> stringResultEntity = mysqlRemoteService.modifyOrderInfoDetailRemote(payOrder);
-                if(!stringResultEntity.isSuccess()){
-                    throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
-                }
-                // 更新项目情况
-                String projectId = payOrder.getProjectId();
-                ResultEntity<Project> projectByProjectIdRemote = mysqlRemoteService.getProjectByProjectIdRemote(projectId);
-                if(!projectByProjectIdRemote.isSuccess()){
-                    throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
-                }
-
-                Project project = projectByProjectIdRemote.getData();
-                Long supporterNumber = project.getSupporterNumber();
-                project.setSupporterNumber(supporterNumber+1);
-                Double supportMoney = project.getSupportMoney();
-                project.setSupportMoney(supportMoney+payOrder.getOrderAmount());
-
-                ResultEntity<String> projectStringResultEntity = mysqlRemoteService.modifyProjectSupporter(payOrder.getUid(), project);
-                if(!projectStringResultEntity.isSuccess()){
-                    throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
-                }
+        if (("TRADE_SUCCESS").equals(tradeStatus)) {
+            // 向前端发送一条支付成功的通知
+            webSocket.sendMessage("true");
+            // 更新订单状态
+            PayOrder payOrder = new PayOrder();
+            payOrder.setId(outTradeNo);
+            payOrder.setPayNum(tradeNo);
+            payOrder.setOrderStatus(CrowdConstant.ORDER_STATUS_PENDING_SHIPMENT);
+            ResultEntity<String> stringResultEntity = mysqlRemoteService.modifyOrderInfoDetailRemote(payOrder);
+            if (!stringResultEntity.isSuccess()) {
+                throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
             }
+            // 更新项目情况
+            ResultEntity<PayOrder> orderInfoDetailRemote = mysqlRemoteService.getOrderInfoDetailRemote(outTradeNo);
+            if (!orderInfoDetailRemote.isSuccess()) {
+                throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
+            }
+            // 重新获取订单数据
+            payOrder = orderInfoDetailRemote.getData();
+
+            String projectId = payOrder.getProjectId();
+
+            ResultEntity<Project> projectByProjectIdRemote = mysqlRemoteService.getProjectByProjectIdRemote(projectId);
+            if (!projectByProjectIdRemote.isSuccess()) {
+                throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
+            }
+
+            Project project = projectByProjectIdRemote.getData();
+            Long supporterNumber = project.getSupporterNumber();
+            project.setSupporterNumber(supporterNumber + 1);
+            Double supportMoney = project.getSupportMoney();
+            project.setSupportMoney(supportMoney + payOrder.getOrderAmount());
+
+            ResultEntity<String> projectStringResultEntity = mysqlRemoteService.modifyProjectSupporter(payOrder.getUid(), project);
+            if (!projectStringResultEntity.isSuccess()) {
+                throw new CrowdException(ResultCodeEnum.NETWORK_ERROR);
+            }
+        }
 //        }else {
 //            //验证失败
 //            //调试用，写文本函数记录程序运行情况是否正常
